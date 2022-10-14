b6fb0be494040f5745078213cd06e8ddc7c042f9
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-14 08:59:26 +0100

    red return_leap_year_when_year_divisible_by_400

--- leap-year-tests/LeapYearShould.cs
+++ leap-year-tests/LeapYearShould.cs
@@ -1,12 +1,25 @@
+using FluentAssertions;
+
 namespace leap_year_tests {
-    public class LeapYearShould {
+    public class LeapYearCalculatorShould {
         [SetUp]
         public void Setup() {
         }
 
         [Test]
-        public void Test1() {
-           
+        public void return_leap_year_when_year_divisible_by_400()
+        {
+            var result = LeapYearCalculator.IsLeapYear(400);
+
+            result.Should().BeTrue();
+        }
+    }
+
+    public class LeapYearCalculator
+    {
+        public static bool IsLeapYear(int year)
+        {
+            throw new NotImplementedException();
         }
     }
 }
