a29debc280e6b27ee4b3ae5dae763a1e778b8945
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-14 08:44:19 +0100

    Agregar archivos de proyecto.

---
+++ leap-year-tests/LeapYearShould.cs
@@ -0,0 +1,12 @@
+namespace leap_year_tests {
+    public class LeapYearShould {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+           
+        }
+    }
+}
