d64c87ed60c6e052d1cd3c9b7445c24d62361688
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-17 07:12:01 +0100

    add test cases

--- leap-year-tests/LeapYearShould.cs
+++ leap-year-tests/LeapYearShould.cs
@@ -8,7 +8,9 @@ namespace leap_year_tests {
         }
 
         [TestCase (400, TestName = "for 400")]
-        [TestCase (400, TestName = "for 800")]
+        [TestCase (800, TestName = "for 800")]
+        [TestCase (2000, TestName = "for 1600")]
+        [TestCase (2000, TestName = "for 2000")]
         public void return_leap_year_when_divisible_by_400(int year)
         {
             var result = LeapYearCalculator.IsLeapYear(year);
@@ -16,26 +18,35 @@ namespace leap_year_tests {
             result.Should().BeTrue();
         }
 
-        [Test]
-        public void return_not_leap_year_when_divisible_by_100_but_not_400()
+        [TestCase (100, TestName = "for 100")]
+        [TestCase (300, TestName = "for 300")]
+        [TestCase (1800, TestName = "for 1800")]
+        [TestCase (1900, TestName = "for 1900")]
+        public void return_not_leap_year_when_divisible_by_100_but_not_400(int year)
         {
-            var result = LeapYearCalculator.IsLeapYear(100);
+            var result = LeapYearCalculator.IsLeapYear(year);
 
             result.Should().BeFalse();
         }
 
-        [Test]
-        public void return_leap_year_when_divisible_by_4_but_not_100()
+        [TestCase (4, TestName = "for 4")]
+        [TestCase (1064, TestName = "for 1064")]
+        [TestCase (2016, TestName = "for 2016")]
+        [TestCase (2020, TestName = "for 20201064")]
+        public void return_leap_year_when_divisible_by_4_but_not_100(int year)
         {
-            var result = LeapYearCalculator.IsLeapYear(4);
+            var result = LeapYearCalculator.IsLeapYear(year);
 
             result.Should().BeTrue();
         }
 
-        [Test]
-        public void return_not_leap_year_when_not_divisible_by_4()
+        [TestCase (3, TestName = "for 3")]
+        [TestCase (566, TestName = "for 563")]
+        [TestCase (1019, TestName = "for 2019")]
+        [TestCase (2021, TestName = "for 2021")]
+        public void return_not_leap_year_when_not_divisible_by_4(int year)
         {
-            var result = LeapYearCalculator.IsLeapYear(3);
+            var result = LeapYearCalculator.IsLeapYear(year);
 
             result.Should().BeFalse();
         }
