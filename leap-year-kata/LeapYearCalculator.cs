540e9567ce1f45fc01825ee48365e62ace6a7f43
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-17 07:03:39 +0100

    green test return_leap_year_when_divisible_by_4_but_not_100

--- leap-year-kata/LeapYearCalculator.cs
+++ leap-year-kata/LeapYearCalculator.cs
@@ -5,7 +5,7 @@ public class LeapYearCalculator
     public static bool IsLeapYear(int year)
     {
         if (year % 400 == 0) return true;
-
+        if (year % 4 == 0 && year % 100 != 0) return true;
         return false;
     }
 }
